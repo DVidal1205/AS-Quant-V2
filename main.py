b4ac106852272febd1bc2f3faddaa33eaa54bc81
Dylan Vidal <dylanvidal1204@gmail.com>
2024-07-22 14:39:59 -0400

    Starts plugging in the as_events function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from methods.preprocess import preprocess_gene_ids
+from methods.as_events import find_as_events
 import configparser
 
 # Read in the configuration values
@@ -6,6 +7,12 @@ config = configparser.ConfigParser()
 config.read("./config/config.ini")
 gtf_file = config["INPUT_FILES"]["gtf_file"]
 ref_file = config["INPUT_FILES"]["ref_file"]
-output_file = config["OUTPUT"]["output_file"]
+as_output_dir = config["OUTPUT"]["as_output_dir"]
+gtf_output_file = config["OUTPUT"]["gtf_output_file"]
+log_dir = config["LOGGING"]["log_dir"]
 
-preprocess_gene_ids(gtf_file, ref_file, output_file)
+# Preprocess the Gene IDs, creating a correct GTF file
+preprocess_gene_ids(gtf_file, ref_file, gtf_output_file)
+
+# Find alternative splicing events with the new GTF file
+find_as_events(gtf_output_file, gtf_output_file.split("/")[-1].split(".")[0], as_output_dir, log_dir)
--- methods/preprocess.py
+++ methods/preprocess.py
@@ -1,19 +1,30 @@
 import sys
+import time
 
 
 def preprocess_gene_ids(gtf, ref, outf):
+    # Step 0: Correct gene_id in GTF file using refFlat file
+    print("\n----------------------------------------------------------------")
+    print("| STEP 0: Correcting gene_id in GTF file using refFlat file... |")
+    print("----------------------------------------------------------------")
+
+    start = time.perf_counter()
+
     # Create a dictionary to store gene_id to gene_name mappings from refFlat file
     hash_dict = {}
     with open(ref, "r") as ref_file:
         for line in ref_file:
             info = line.split()
-            # info[1] is gene_id and info[12] is gene_name in refFlat file
-            hash_dict[info[0]] = info[11]
+            # info[1] is gene_id and info[0] is gene_name in refFlat file
+            hash_dict[info[1]] = info[0].replace('"', "").replace(";", "")
+    # print(hash_dict)
 
     # Create a dictionary to record transcripts that don't have gene ID in refFlat
     list_dict = {}
 
     # Open the GTF file for reading and the output file for writing
+    count = 0
+    drop_count = 0
     with open(gtf, "r") as gtf_file, open(outf, "w") as out_file:
         for line in gtf_file:
             line = line.strip()  # Remove leading/trailing whitespace
@@ -24,14 +35,22 @@ def preprocess_gene_ids(gtf, ref, outf):
                 gene_id = ""
                 # Extract gene_id and transcript_id from the attributes field (info[8])
                 g_id = info[8].split('gene_id "')[1].split('";')[0]
+                g_id = g_id.split(".")[0]
+                # print("Gene ID Extracted:", g_id)
+
+                # Check for XM, XR, and YP prefixes in gene_id, and filter them out
+                if g_id.startswith("XM") or g_id.startswith("XR") or g_id.startswith("YP"):
+                    drop_count += 1
+                    continue
 
                 # If gene_id is in refFlat hash dictionary, use the corresponding gene_name
                 if g_id in hash_dict:
+                    # print("Gene ID Found in refFlat, correcting:", gene_id)
                     gene_id = hash_dict[g_id]
                 else:
-                    # If gene_id is not in refFlat, keep the original gene_id
-                    gene_id = g_id
-                    list_dict[g_id] = 1  # Record the missing gene_id
+                    # If gene_id is not in refFlat, drop the transcript
+                    drop_count += 1
+                    continue
 
                 # Update the attributes field with the new or original gene_id
                 info[8] = f'gene_id "{gene_id}"; transcript_id "{g_id}";'
@@ -40,12 +59,16 @@ def preprocess_gene_ids(gtf, ref, outf):
                 sys.exit(1)
 
             # Write the updated line to the output file
+            count += 1
             out_file.write("\t".join(info) + "\n")
 
     # Print the number of transcripts without a gene ID to stderr
     n = len(list_dict)
-    print(f"There are {n} transcripts that don't have gene id.", file=sys.stderr)
+    print(f"{count} transcripts processed ", file=sys.stderr)
+    print(f"{drop_count} transcripts dropped ", file=sys.stderr)
+    print(f"Step 0 Completed in {time.perf_counter() - start:.2f} seconds")
+    print("----------------------------------------------------------------\n")
 
-    # Print each missing gene_id to stderr
-    for key in list_dict:
-        print(f"~{key}~", file=sys.stderr)
+    # # Print each missing gene_id to stderr
+    # for key in list_dict:
+    #     print(f"~{key}~", file=sys.stderr)
